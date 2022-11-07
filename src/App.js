51a59098fea532b7e46491f51b44978fdc59dbdb
chandranatha <chinthachandranatha963@gmail.com>
2022-11-07 22:51:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Button } from 'bootstrap';
+import React,{useState} from 'react';
+import './App.css'
+const App = () => {
+  const [city, setCity] = useState('');
+  const [result, setResult] = useState({
+    cityName:'',
+    temperature : ''
+  });
+  
+  const handleWeatherBtn = ()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      res=>res.json()
+    )?.then((data)=>{
+      const kelvin = data.main.temp;
+      const celsius = kelvin-273.15;
+      setResult({cityName:data.name,temperature:Math.round(celsius)+ '°C'});
+      
+    })
+      
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+       <div className='card' style={{height:400}}>
+       <center>
+       <h1 className='card-title'>Weather</h1>
+        <input type='text' 
+              className='search' 
+              value={city} 
+              placeholder='Enter Your City Name'
+              onChange={(e)=>setCity(e.target.value)}></input> <br /><br />
+        <button className='btn btn-primary' onClick={handleWeatherBtn}>Find Weather</button>
+       
+                <h5 className='city'>{result.cityName}</h5>
+                <h1 className='temp'>{result.temperature} </h1>
+              
+           
+        </center>
+       </div>
+      </center>
     </div>
   );
 }
